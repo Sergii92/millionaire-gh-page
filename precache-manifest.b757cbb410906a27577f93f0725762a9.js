477c33a0f57b86c80ff757731986660c7501ed57
Sergii <sergii.mykhailyk@gmail.com>
2020-10-04 11:54:49 +0300

    Updates

---
+++ precache-manifest.b757cbb410906a27577f93f0725762a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a86698078e60d774d2f4d362cc87e3",
+    "url": "/millionaire-gh-page/index.html"
+  },
+  {
+    "revision": "1c64dc1dc79182e702e4",
+    "url": "/millionaire-gh-page/static/css/main.8d372884.chunk.css"
+  },
+  {
+    "revision": "8119c41b87b0172d556a",
+    "url": "/millionaire-gh-page/static/js/2.81e183ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/millionaire-gh-page/static/js/2.81e183ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c64dc1dc79182e702e4",
+    "url": "/millionaire-gh-page/static/js/main.454af3bb.chunk.js"
+  },
+  {
+    "revision": "700b10c460ede2432b4e",
+    "url": "/millionaire-gh-page/static/js/runtime-main.ae73494f.js"
+  },
+  {
+    "revision": "c7cf242a543ae2b7b7cfd4cfe7087f14",
+    "url": "/millionaire-gh-page/static/media/Rectangle.c7cf242a.png"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/millionaire-gh-page/static/media/hand1.f8c75481.png"
+  }
+]);
